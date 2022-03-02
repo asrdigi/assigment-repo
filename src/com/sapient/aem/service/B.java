f98f80de269ec7dc489657c8617e2aca508c60be
Annapureddy Srinivas Reddy <digitech1993@gmail.com>
2022-03-02 15:20:28 +0530

    first commit

---
+++ src/com/sapient/aem/service/B.java
@@ -0,0 +1,48 @@
+package com.sapient.aem.service;
+
+public class B extends A{
+	private int b;
+	
+	public B() {
+		
+	}
+	
+	public B(int b) {
+		super(); //implicit, invoking default constructor of super class
+		this.b=b;
+	}
+	
+	/*
+	 * To invoke parameterized constructor methods of super class,
+	 * use super() in subclass constructor methods
+	 */
+	public B(int a, int b) {
+		//if the following statement is not written, the default constructor of
+		//super class is invoked
+		super(a); //invoking 1-arg constructor method of super class
+		this.b=b;
+		
+	}
+
+	public int getB() {		
+		return b;
+	}
+	
+	
+	
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	@Override
+	public void greet() {		
+		super.greet(); //invokes greet() of class A
+		System.out.println("Method overriding");
+	}
+	
+	
+	
+	
+	
+}
